75136e75e9d71a4bca1afef7f794080001a15c8a
Connor Stevens <conmail1224@gmail.com>
2022-12-18 16:51:08 -0500

    Delete complicated annoyance

--- packages/app/main/graphql/resolvers/settings.ts
+++ packages/app/main/graphql/resolvers/settings.ts
@@ -1,8 +1,6 @@
-import { Settings } from '@local/graphql/dist';
 import { read, write } from '@local/data/schemas/settings';
-import { BaseResolver } from '../../types/base-resolver';
 
-const resolvers: BaseResolver<Settings> = {
+const resolvers = {
   queries: {
     settings: async function getSettings() {
       const settings = await read();
